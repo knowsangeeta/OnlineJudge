99c9472728590dee233c329cf104fe859350a82e
Sangeeta <ksange0301@gmail.com>
2023-11-26 22:10:40 +0530

    login and register fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,31 +5,31 @@ import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 
 function App() {
-  const currentUser = true;
+  const { currentUser } = useContext(Context);
   return (
-    // <Router>
-    //   <Topbar />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route
-    //       path="/register"
-    //       element={currentUser ? <Home /> : <Register />}
-    //     />
-    //     <Route
-    //       path="/login"
-    //       element={currentUser ? <Home /> : <Login />}
-    //     />
-    //     <Route
-    //       path="/settings"
-    //       element={currentUser ? <Settings /> : <Login />}
-    //     />
-    //   </Routes>
-    // </Router>
-<Login />
-    
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/register"
+          element={currentUser ? <Home /> : <Register />}
+        />
+        <Route
+          path="/login"
+          element={currentUser ? <Home /> : <Login />}
+        />
+        <Route
+          path="/settings"
+          element={currentUser ? <Settings /> : <Login />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
